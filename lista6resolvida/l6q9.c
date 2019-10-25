ae53905547792db733b0a6f58cc4895227dd9bca
JeffersonVictorAds <57008798+JeffersonVictorAds@users.noreply.github.com>
2019-10-25 13:15:06 -0300

    Create l6q9.c

---
+++ lista6resolvida/l6q9.c
@@ -0,0 +1,14 @@
+  #include <stdio.h>
+int main(){
+int n;
+char name[30];
+int l=0;
+printf("digite seu numero: ");
+gets(name);
+for (n=0;name[n]!='\0';n++){
+if(name[n]=='1'){
+l++;
+}
+}
+printf("Seu numero tem %d 1's",l);
+}
